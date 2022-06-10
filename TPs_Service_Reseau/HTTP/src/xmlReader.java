b4bb1cfe52888ef0d154fd14aecf0cbdacdcf527
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2022-06-10 10:18:27 +0200

    ajoute xml

--- TPs_Service_Reseau/HTTP/src/xmlReader.java
+++ TPs_Service_Reseau/HTTP/src/xmlReader.java
@@ -13,13 +13,27 @@ import java.io.IOException;
 
 public class xmlReader {
 
+    private String port;
+    private String racine;
+    private String index;
+    private String accept;
+
     final static String  root = "Tps_Service_Reseau/HTTP/src/myweb.conf";
+
+    public xmlReader(){
+        this.port="80";
+        this.racine="./doc/";
+        this.index="true";
+        this.accept="localhost";
+    }
 
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         File file = new File(root);
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document document = db.parse(file);
+
+        xmlReader xmlReader = new xmlReader();
 
         document.getDocumentElement().normalize();
         System.out.println("Root Element :" + document.getDocumentElement().getNodeName() + "\n");
@@ -30,20 +44,48 @@ public class xmlReader {
             System.out.println("\nCurrent Element : " + nNode.getNodeName());
             if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                 Element eElement = (Element) nNode;
-                System.out.println("Port: " + eElement.getElementsByTagName("port").item(0).getTextContent());
-                System.out.println("Root: " + eElement.getElementsByTagName("root").item(0).getTextContent());
-                System.out.println("Index: " + eElement.getElementsByTagName("index").item(0).getTextContent());
-                System.out.println("Accept: " + eElement.getElementsByTagName("accept").item(0).getTextContent());
+                xmlReader.setPort(eElement.getElementsByTagName("port").item(0).getTextContent());
+                System.out.println("Port: " + xmlReader.getPort());
+                xmlReader.setRacine(eElement.getElementsByTagName("root").item(0).getTextContent());
+                System.out.println("Root: " + xmlReader.getRacine());
+                xmlReader.setIndex(eElement.getElementsByTagName("index").item(0).getTextContent());
+                System.out.println("Index: " + xmlReader.getIndex());
+                xmlReader.setAccept(eElement.getElementsByTagName("accept").item(0).getTextContent());
+                System.out.println("Accept: " + xmlReader.getAccept());
             }
         }
     }
 
-
-
-
-
-    public xmlReader() throws ParserConfigurationException, IOException, SAXException {
+    public String getPort() {
+        return port;
     }
 
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getRacine() {
+        return racine;
+    }
+
+    public void setRacine(String racine) {
+        this.racine = racine;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getAccept() {
+        return accept;
+    }
+
+    public void setAccept(String accept) {
+        this.accept = accept;
+    }
 }
 
